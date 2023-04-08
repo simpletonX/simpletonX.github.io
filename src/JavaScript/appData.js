9b2caa6ad2993c54f8b7186c55461176a9fda6c8
simpletonX <130218586+simpletonX@users.noreply.github.com>
2023-04-08 20:20:13 +0800

    1

--- src/JavaScript/appData.js
+++ src/JavaScript/appData.js
@@ -3,7 +3,7 @@ const applicationData = [
     type: "Brief introduction",
     title: "Introduction to Alligator and the Author",
     range: "顶置",
-    image: "./../../src/images/app-data/01.jpg",
+    image: "",
     link: "./page/index.html",
     update_time: "2023-04-07"
   },
@@ -11,7 +11,7 @@ const applicationData = [
     type: "HTML Fundamentals",
     title: "HTML在线文档（从零基础到入门）",
     range: "开放",
-    image: "./../../src/images/app-data/02.jpg",
+    image: "",
     link: "",
     update_time: "2023-04-07"
   },
@@ -19,7 +19,7 @@ const applicationData = [
     type: "CSS Fundamentals",
     title: "CSS在线文档（从零基础到入门）",
     range: "开放",
-    image: "./../../src/images/app-data/03.jpg",
+    image: "",
     link: "",
     update_time: "2023-04-08"
   },
@@ -27,63 +27,69 @@ const applicationData = [
     type: "HTML & CSS",
     title: "HTML&CSS静态页面项目实战资料",
     range: "未开放",
-    image: "./../../src/images/app-data/04.jpg",
+    image: "",
     update_time: "2023-04-18"
   },
   {
     type: "HTML Proficiency",
     title: "HTML在线文档（从入门到精通）",
     range: "未开放",
-    image: "./../../src/images/app-data/05.jpg",
+    image: "",
     update_time: "2023-04-26"
   },
   {
     type: "CSS Proficiency",
     title: "CSS在线文档（从入门到精通）",
     range: "未开放",
-    image: "./../../src/images/app-data/06.jpg",
+    image: "",
     update_time: "2023-05-01"
   },
   {
     type: "JavaScript Basic",
     title: "JavaScript在线文档（ECMAScript）",
     range: "未开放",
-    image: "./../../src/images/app-data/07.jpg",
+    image: "",
     update_time: "--"
   },
   {
     type: "Object Model",
     title: "JavaScript在线文档（对象模型接口）",
     range: "未开放",
-    image: "./../../src/images/app-data/08.jpg",
+    image: "",
     update_time: "--"
   },
   {
     type: "Asynchronous",
     title: "AJAX异步请求（前后端数据交互）",
     range: "未开放",
-    image: "./../../src/images/app-data/09.jpg",
+    image: "",
     update_time: "--"
   },
   {
     type: "Dynamic website",
     title: "PHP动态网站开发（快速上手）",
     range: "未开放",
-    image: "./../../src/images/app-data/10.jpg",
+    image: "",
     update_time: "--"
   },
   {
     type: "Interface design",
     title: "WEB UI交互设计(Author独门秘籍)",
     range: "独家",
-    image: "./../../src/images/app-data/11.jpg",
+    image: "",
     update_time: "--"
   },
   {
     type: "Logical Skills",
     title: "JavaScript&CSS逻辑交互技巧",
     range: "独家",
-    image: "./../../src/images/app-data/12.jpg",
+    image: "",
     update_time: "--"
   }
 ]
+
+applicationData.forEach((ele, index) => {
+  ele.image = `https://cdn.jsdelivr.net/gh/simpletonX/simpletonX.github.io@main/src/images/app-data/${
+    index < 10 ? "0" + index : index
+  }.jpg`
+})
